d4ed05297f46c8f7d8d88557488c119c4c0af5bb
Starlis <72180007+starlisdet@users.noreply.github.com>
2023-03-14 20:52:43 -0300

    Add files via upload

---
+++ www/js/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC5weZPorCsMRCjXWBOyb5IcJpPX7v8d3g",
+  authDomain: "projeto-rpg-f8ffe.firebaseapp.com",
+  databaseURL: "https://projeto-rpg-f8ffe-default-rtdb.firebaseio.com",
+  projectId: "projeto-rpg-f8ffe",
+  storageBucket: "projeto-rpg-f8ffe.appspot.com",
+  messagingSenderId: "347306114003",
+  appId: "1:347306114003:web:571b988376dfae558bcd26"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ www/js/register.js
@@ -0,0 +1,36 @@
+var app = {
+        
+  // Application Constructor
+  initialize: function() {
+      document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+  },
+
+  onDeviceReady: function() {
+      document.getElementById("btnInserir").addEventListener("click",app.inserir);
+  },
+
+  inserir: function(){
+      event.preventDefault();
+      
+      let cname = document.getElementById("name").value;
+      let cemail = document.getElementById("email").value;
+      let cpassword = document.getElementById("password").value;
+      
+      var db = firebase.firestore();
+
+      db.collection("user").add({
+          name: cname,
+          email: cemail,
+          password: cpassword,
+      })
+      .then((docRef) =>{
+          window.location.href = cordova.file.applicationDirectory + "www/index.html";
+      })
+      .catch((error) => {
+          console.error("Error adding document: ", error);
+      });
+  }
+  
+};
+
+app.initialize();
