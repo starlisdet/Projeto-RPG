08951373c31b9bb9c5640b90d6eacd619b292dbd
Yann Rodrigues Casanova <yann.casanova1607@gmail.com>
2023-04-11 20:48:46 -0300

    sql

--- dbc/main.sql
+++ dbc/main.sql
@@ -1,8 +1,7 @@
 -- Active: 1681252546488@@127.0.0.1@3306
 create table [projetorpg].[user] {
-    usrid int not null auto increment PRIMARY KEY,
+    usrid int auto_increment PRIMARY KEY,
     name VARCHAR(30),
-    username VARCHAR(15),
     email VARCHAR(40),
     password VARCHAR(30),
 };
