201a97d694aedd5020f8b264416851f6c6180781
Yann Rodrigues Casanova <yann.casanova1607@gmail.com>
2023-04-18 19:56:20 -0300

    crud update

--- app/js/usercrud.js
+++ app/js/usercrud.js
@@ -1,106 +1,98 @@
-const form = document.querySelector('#user-form');
-const userList = document.querySelector('#user-list');
 
-let users = [];
-
-// Create user
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  const name = document.querySelector('#name').value.trim();
-  const email = document.querySelector('#email').value.trim();
-  const password = document.querySelector('#password').value.trim();
-  const id = document.querySelector('#id').value;
-
-  if (!name || !email) {
-    alert('Please provide a name and email.');
-    return;
-  }
-
-  const user = {
-    name,
-    email,
-    password,
-    id: id || new Date().getTime().toString()
-  };
-
-  if (id) {
-    const index = users.findIndex(u => u.id === id);
-    users.splice(index, 1, user);
-    clearForm();
-  } else {
-    users.push(user);
-    clearForm();
-  }
-
-  renderUsers();
-
-var express = require('express');
-var router = express.Router();
-var db=require('../dbc');
-router.get('/form', function(req, res, next) { 
-res.render('users'); 
-});
-router.post('/create', function(req, res, next) {
-  
-  // store all the user input data
-  const userDetails=req.body;
- 
-  // insert user data into users table
-  var sql = 'INSERT INTO users SET ?';
-  db.query(sql, userDetails,function (err, data) { 
-      if (err) throw err;
-         console.log("User dat is inserted successfully "); 
+// Create a new user
+const createUser = (user, callback) => {
+  const { name, email, password } = user;
+  const query = 'INSERT INTO users (name, email, password) VALUES (?, ?, ?)';
+  connection.query(query, [name, email, password], (error, result) => {
+    if (error) {
+      console.error('Error creating user:', error);
+      callback(error);
+      return;
+    }
+    console.log('User created successfully');
+    callback(null, result);
   });
- res.redirect('/users/form');  // redirect to user form page after inserting the data
-}); 
-module.exports = router;
+};
 
-});
-
-// Read users
-function renderUsers() {
-  userList.innerHTML = '';
-  users.forEach(user => {
-    const tr = document.createElement('tr');
-    const tdName = document.createElement('td');
-    const tdEmail = document.createElement('td');
-    const tdPassword = document.createElement('td');
-    const tdActions = document.createElement('td');
-    const editButton = document.createElement('button');
-    const deleteButton = document.createElement('button');
-
-    tdName.textContent = user.name;
-    tdEmail.textContent = user.email;
-    tdPassword.textContent = user.password || '-';
-    editButton.textContent = 'Edit';
-    editButton.addEventListener('click', () => {
-      document.querySelector('#name').value = user.name;
-      document.querySelector('#email').value = user.email;
-      document.querySelector('#password').value = user.password || '';
-      document.querySelector('#id').value = user.id;
-    });
-
-    //delete user
-    deleteButton.textContent = 'Delete';
-    deleteButton.addEventListener('click', () => {
-      const index = users.findIndex(u => u.id === user.id);
-      users.splice(index, 1);
-      renderUsers();
-    });
-
-    tdActions.appendChild(editButton);
-    tdActions.appendChild(deleteButton);
-
-    tr.appendChild(tdName);
-    tr.appendChild(tdEmail);
-    tr.appendChild(tdPassword);
-    tr.appendChild(tdActions);
-
-    userList.appendChild(tr);
+// Get all users
+const getAllUsers = callback => {
+  const query = 'SELECT * FROM users';
+  connection.query(query, (error, results) => {
+    if (error) {
+      console.error('Error getting users:', error);
+      callback(error);
+      return;
+    }
+    console.log('Users retrieved successfully');
+    callback(null, results);
   });
-}
+};
 
-// Update user
-function clearForm() {
-  document.querySelector('#user-form').reset;
-}
+// Get a user by ID
+const getUserById = (usrid, callback) => {
+  const query = 'SELECT * FROM users WHERE id = ?';
+  connection.query(query, [usrid], (error, result) => {
+    if (error) {
+      console.error('Error getting user:', err);
+      callback(error);
+      return;
+    }
+    if (result.length === 0) {
+      console.error('User not found');
+      callback(new Error('User not found'));
+      return;
+    }
+    console.log('User retrieved successfully');
+    callback(null, result[0]);
+  });
+};
+
+// Update a user by ID
+const updateUserById = (usrid, user, callback) => {
+  const { name, age, email } = user;
+  const query = 'UPDATE users SET name = ?, email = ?, password = ? WHERE id = ?';
+  connection.query(query, [name, email, password, userId], (err, result) => {
+    if (err) {
+      console.erroror('Error updating user:', error);
+      callback(error);
+      return;
+    }
+    console.log('User updated successfully');
+    callback(null, result);
+  });
+};
+
+// Delete a user by ID
+const deleteUserById = (userId, callback) => {
+  const query = 'DELETE FROM users WHERE id = ?';
+  connection.query(query, [userId], (error, result) => {
+    if (error) {
+      console.erroror('Error deleting user:', error);
+      callback(error);
+      return;
+    }
+    console.log('User deleted successfully');
+    callback(null, result);
+  });
+};
+
+// Close the database connection
+const closeConnection = () => {
+  connection.end(error => {
+    if (error) {
+      console.erroror('Error closing database connection:', error);
+      return;
+    }
+    console.log('Database connection closed');
+  });
+};
+
+// Export the CRUD functions
+module.exports = {
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUserById,
+  deleteUserById,
+  closeConnection
+};
